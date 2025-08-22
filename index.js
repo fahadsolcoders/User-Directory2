379ca5be002ed661bcd4f807ae924d148848d533
fahad <fahad@gmail.com>
2025-08-22 16:36:48 +0500

    user directory

--- index.js
+++ index.js
@@ -1,142 +1,145 @@
-let user =  []
-let filtered = []
-let currentPage = 1
-const itemPerPage = 5
+let users = [];
+let filteredUsers = [];
+let currentPage = 1;
+const itemsPerPage = 5;
 
-const userGrid =  document.getElementById("userGrid")
-const searchInput = document.getElementById("searchInput")
-const sortSelect = document.getElementById("sortSelect")
-const pagination = document.getElementById("pagination")
-const loading = document.getElementById("loading")
-const noResult = document.getElementById("noResult")
-const modal = document.getElementById("userModal")
-const closeBtn = document.querySelector(".close")
-const themeToggle = document.getElementById("themeToggle")
+const userGrid = document.getElementById("userGrid");
+const searchInput = document.getElementById("searchInput");
+const sortSelect = document.getElementById("sortSelect");
+const pagination = document.getElementById("pagination");
+const loading = document.getElementById("loading");
+const noResults = document.getElementById("noResults");
+const modal = document.getElementById("userModal");
+const closeBtn = document.querySelector(".close");
+const themeToggle = document.getElementById("themeToggle");
 
-window.addEventListener("load",fetchUsers)
-searchInput.addEventListener("input",handleSearch)
-sortSelect.addEventListener("change",handelsort)
-closeBtn.addEventListener("click",closeModal)
-window.addEventListener("click",(e) => {
-    if(e.target === modal)
-        closeModal()
-})
-themeToggle.addEventListener("click",themeToggle)
+window.addEventListener("load", fetchUsers);
+searchInput.addEventListener("input", handleSearch);
+sortSelect.addEventListener("change", handleSort);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", (e) => {
+  if (e.target === modal) closeModal();
+});
+themeToggle.addEventListener("click", toggleTheme);
 
-async function fetchUsers(){
-    loading.style.display = "block";
-    try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const users = await response.json();
-        filteredUsers = [...users]
-        displayUsers()
-    }
-    catch(error){
-        console.error(`Error fetching users:`,error);
-    }
-    finally{
-        loading.style.display = "none"
-    }
+async function fetchUsers() {
+  loading.style.display = "block";
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    users = await response.json();
+    filteredUsers = [...users];
+    displayUsers();
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  } finally {
+    loading.style.display = "none";
+  }
 }
 
-function displayUsers(){
-    userGrid.innerHTML = ""
-    noResult.style.display = "none"
+function displayUsers() {
+  userGrid.innerHTML = "";
+  noResults.style.display = "none";
 
-    if(filteredUsers.length === 0){
-        noResult.style.display = "block"
-        pagination.innerHTML = "";
-        return
-    }
+  if (filteredUsers.length === 0) {
+    noResults.style.display = "block";
+    pagination.innerHTML = "";
+    return;
+  }
 
-    const start = (currentPage - 1) * itemPerPage
-    const end = start + itemsPerPage
-    const paginatedUsers = filteredUsers.slice(start,end)
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedUsers = filteredUsers.slice(start, end);
 
-    paginatedUsers.forEach(user => {
-        const card = document.createElement("div")
-        card.classList.add("user-card")
-        card.innerHTML=`
-        <h3>${user.name}</h3>
-        <p>Username: ${user.username}</p>
-        <p>Email: ${user.email}</p>
-        <p>Comapany: ${user.company.name}</p>
-        `
-        card.addEventListener("click", () => openModal(user));
-        userGrid.appendChild(card)
-    })
-    setupPagination()
+  paginatedUsers.forEach((user) => {
+    const card = document.createElement("div");
+    card.classList.add("user-card");
+    card.innerHTML = `
+            <h3>${user.name}</h3>
+            <p>Username: ${user.username}</p>
+            <p>Email: ${user.email}</p>
+            <p>Company: ${user.company.name}</p>
+        `;
+    card.addEventListener("click", () => openModal(user));
+    userGrid.appendChild(card);
+  });
+
+  setupPagination();
 }
 
 function setupPagination() {
-    pagination.innerHTML = ""
-    const pageCount = Math.ceil(filteredUsers.length / itemsPerPage)
+  pagination.innerHTML = "";
+  const pageCount = Math.ceil(filteredUsers.length / itemsPerPage);
 
-    const prevBtn = document.createElement("button");
-    prevBtn.textContent = "Previous"
-    prevBtn.classList.toggle("disabled",currentPage === 1)
-    prevBtn.addEventListener("click",() => {
-        if(currentPage > 1){
-            currentPage--;
-            displayUsers()
-        }
-    })
-    pagination.appendChild(prevBtn)
-
-    const nextBtn = document.createElement("button")
-    nextBtn.textContent = "Next"
-    nextBtn.classList.toggle("diabled",currentPage === pageCount);
-    nextBtn.addEventListener("click", () =>{
-        if(currentPage < pageCount){
-            currentPage++
-            displayUsers()
-        }
-    })
-    pagination.appendChild(nextBtn)
-}
-
-function handleSearch(){
-    const query = searchInput.ariaValueMax.toLocaleLowerCase()
-    filteredUsers = users.filter(user => user.name.toLocaleLowerCase().includes(query) || user.username.toLocaleLowerCase().includes(query))
-    currentPage = 1
-    handelsort();
-}
-
-function handelsort(){
-    const sortValue = sortSelect.value
-    if(sortValue === "asc"){
-        filteredUsers.sort((a,b) => a.name.localeCompare(b.name))
+  const prevBtn = document.createElement("button");
+  prevBtn.textContent = "Previous";
+  prevBtn.classList.toggle("disabled", currentPage === 1);
+  prevBtn.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      displayUsers();
     }
-    else if(sortValue === "desc"){
-        filteredUsers.sort((a,b) => b.name.localeCompare(a.name))
+  });
+  pagination.appendChild(prevBtn);
+
+  const nextBtn = document.createElement("button");
+  nextBtn.textContent = "Next";
+  nextBtn.classList.toggle("disabled", currentPage === pageCount);
+  nextBtn.addEventListener("click", () => {
+    if (currentPage < pageCount) {
+      currentPage++;
+      displayUsers();
     }
-    currentPage = 1
-    displayUsers()
+  });
+  pagination.appendChild(nextBtn);
 }
 
-function openModal(user){
-    document.getElementById("modalName").textContent = user.name;
-    document.getElementById("modalUsername").textContent = user.username
-    document.getElementById("modalPhone").textContent = user.phone
-    document.getElementById("modalAddress").textContent = `${user.address.street},${user.address.city},${user.address,zipcode}`
-    document.getElementById("modalCompany").textContent = user.company.name
-    modal.style.display = "flex"
+function handleSearch() {
+  const query = searchInput.value.toLowerCase();
+  filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(query) ||
+      user.username.toLowerCase().includes(query)
+  );
+  currentPage = 1;
+  handleSort();
+}
+
+function handleSort() {
+  const sortValue = sortSelect.value;
+  if (sortValue === "asc") {
+    filteredUsers.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortValue === "desc") {
+    filteredUsers.sort((a, b) => b.name.localeCompare(a.name));
+  }
+  currentPage = 1;
+  displayUsers();
+}
+
+function openModal(user) {
+  document.getElementById("modalName").textContent = user.name;
+  document.getElementById("modalUsername").textContent = user.username;
+  document.getElementById("modalEmail").textContent = user.email;
+  document.getElementById("modalPhone").textContent = user.phone;
+  document.getElementById(
+    "modalAddress"
+  ).textContent = `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`;
+  document.getElementById("modalCompany").textContent = user.company.name;
+  modal.style.display = "flex";
 }
 
 function closeModal() {
-    modal.style.display = "none" 
+  modal.style.display = "none";
 }
 
-function toggleTheme(){
-    document.body.classList.toggle("dark")
-    if(document.body.classList.contains("dark")){
-        localStorage.setItem("theme","dark")
-    }
-    else{
-        localStorage.removeItem("theme")
-    }
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.removeItem("theme");
+  }
 }
 
-if(localStorage.getItem("theme") === "dark"){
-    document.body.classList.add("dark")
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
 }
